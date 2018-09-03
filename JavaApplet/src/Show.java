c2e905a59b943edebbdda20e3d15aaac3af7c3f3
Snguyen2 <42800653+Snguyen2@users.noreply.github.com>
2018-09-03 08:58:18 +0200

    opdracht 2.1 Applet

--- JavaApplet/src/Show.java
+++ JavaApplet/src/Show.java
@@ -5,11 +5,11 @@ import java.awt.*;
 public class Show extends Applet {
     //een (lege) methode die de Applet initialiseert
     public void init() {
-        setBackground(new Color(248, 255, 247));
+        setBackground(new Color(23, 20, 255));
     }
     //een methode die de inhoud van het scherm tekent
     public void paint(Graphics g) {
-        g.setColor(new Color(14, 16, 128));
+        g.setColor(new Color(254, 255, 10));
         g.drawString("Mijn eerste Applet", 50, 70 );
 
     }
